5bf1ca3c58e4d29b8a990ec416654d4c3cee180a
Fasulyak Dmytro <fasulyak03@gmail.com>
2021-04-12 23:17:50 +0300

    Commit

---
+++ Kuba/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+
+  $('.photo__slider').slick({
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/Shape (1).svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/Shape.svg" alt=""></button>'
+  });
+
+});
